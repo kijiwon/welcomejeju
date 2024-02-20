e58fa00d44b17d2c49fb3c3be8a43d9a278f8500
kijiwon <kiji1313@gmail.com>
2024-02-20 19:22:21 +0900

    swiper 이미지 추가

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -6,8 +6,27 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import styles from "../styles/slider.module.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function Slider() {
+  const [data, setData] = useState([]);
+  const getData = async () => {
+    const res = await fetch(
+      "https://api.visitjeju.net/vsjApi/contents/searchList?apiKey=7588431ws84if4kh&locale=kr"
+    );
+
+    const json = await res.json();
+    const items = json.items.filter(
+      (it) => it.contentscd.label === "축제/행사"
+    );
+    setData(items.slice(5, 9));
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  console.log(data);
   return (
     <div className={styles.slide_wrapper}>
       <Swiper
@@ -15,20 +34,24 @@ export default function Slider() {
         spaceBetween={30}
         centeredSlides={true}
         autoplay={{
-          delay: 3500,
+          delay: 2500,
           disableOnInteraction: false,
+          pauseOnMouseEnter: true,
         }}
-        loop={true}
         pagination={{
           clickable: true,
         }}
         navigation={true}
         modules={[Autoplay, Pagination, Navigation]}
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
+        {data.length !== 0 &&
+          data.map((it) => (
+            <SwiperSlide key={it.contentsid}>
+              <div className={styles.img_wrapper}>
+                <img src={it.repPhoto.photoid.imgpath} />
+              </div>
+            </SwiperSlide>
+          ))}
       </Swiper>
     </div>
   );
